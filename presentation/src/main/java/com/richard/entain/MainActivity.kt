102f0d1f7ce28ae105566a96ba83b2ccefdce06a
Richard Lai <richard08192@hotmail.com>
2024-03-06 18:46:30 +1000

    Add Hilt annotation to MainActivity

--- presentation/src/main/java/com/richard/entain/MainActivity.kt
+++ presentation/src/main/java/com/richard/entain/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.richard.entain.racing.RacingActivity
 import com.richard.entain.ui.theme.EntainTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         fun navigateToRacingScreen() {
