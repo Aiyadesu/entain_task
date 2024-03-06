0b2361f5a7e3c9f12026e3d788effb36a3a085fc
Richard Lai <richard08192@hotmail.com>
2024-03-06 18:45:11 +1000

    Add Hilt annotations to RetrofitClient

--- data/src/main/java/com/richard/data/RetrofitClient.kt
+++ data/src/main/java/com/richard/data/RetrofitClient.kt
@@ -1,12 +1,19 @@
 package com.richard.data
 
 import com.richard.data.racing.service.RacingApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@Module
+@InstallIn(SingletonComponent::class)
 object RetrofitClient {
     private const val BASE_URL = "https://api.neds.com.au/rest/v1/"
 
+    @Provides
     fun createRaceApiService(): RacingApiService {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
