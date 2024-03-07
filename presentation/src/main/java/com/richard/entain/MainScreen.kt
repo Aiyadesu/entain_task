1751a826047651be351c5b22d4e70a9628028807
Richard Lai <richard08192@hotmail.com>
2024-03-07 15:45:18 +1000

    Fix call to action text not centering when font size is large

--- presentation/src/main/java/com/richard/entain/MainScreen.kt
+++ presentation/src/main/java/com/richard/entain/MainScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -19,7 +20,11 @@ fun MainScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(stringResource(R.string.main_screen_description))
+        Text(
+            text = stringResource(R.string.main_screen_description),
+            textAlign = TextAlign.Center,
+        )
+
         Button(
             modifier = Modifier
                 .padding(4.dp),
